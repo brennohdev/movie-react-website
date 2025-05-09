01574f7b7c1e7469a962bb70e300e7aa620cdac1
Brenno Henrique <brennohdev@gmail.com>
2025-05-08 22:24:58 -0300

    changes

--- react-app/src/components/movie/MovieCard.jsx
+++ react-app/src/components/movie/MovieCard.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language } }) => {
   return (
     <div className="movie-card">
-      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : './no-movie.png'} alt="Movie card with informations"/>
+      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : './public/no-movie.png'} alt="Movie card with informations"/>
       <div className="mt-4">
         <h3>{title}</h3>
         <div className="content">
           <div className="rating">
-            <img src="/star.svg" alt="Star Icon"/>
+            <img src="./public/star.svg" alt="Star Icon"/>
             <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
           </div>
           <span>•</span>
