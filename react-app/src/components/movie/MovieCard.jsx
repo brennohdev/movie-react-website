f88b7d7643c8443ec5412f7aa701fc92992e67c5
Brenno Henrique <brennohdev@gmail.com>
2025-05-08 22:33:06 -0300

    changes

--- react-app/src/components/movie/MovieCard.jsx
+++ react-app/src/components/movie/MovieCard.jsx
@@ -8,7 +8,7 @@ const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, or
         <h3>{title}</h3>
         <div className="content">
           <div className="rating">
-            <img src="./star.svg" alt="Star Icon"/>
+            <img src="/star.svg" alt="Star Icon"/>
             <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
           </div>
           <span>•</span>
